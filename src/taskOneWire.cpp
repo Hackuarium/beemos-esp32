03cab3a4ef865e727a845e95782e99e571a54b2a
Luc Patiny <luc@patiny.com>
2021-10-05 19:11:26 +0200

    chore: simplify taskOneWire

--- src/taskOneWire.cpp
+++ src/taskOneWire.cpp
@@ -16,38 +16,16 @@ void TaskOneWire(void* pvParameters) {
   
   
   while (true) {
-<<<<<<< HEAD
-    
-	  sensors_oneWire.requestTemperatures();
 
-
-    int last_temp1 = sensors_oneWire.getTempCByIndex(0)*100;
-   // vTaskDelay(1000);
-    // Define filtering of "-127" values, using a maximum number of tries before really assigning -127
-    byte tries = 0; 
-    byte max_tries=5;
-    while((last_temp1==0||last_temp1==-12700) && tries<max_tries){
-      last_temp1 = sensors_oneWire.getTempCByIndex(0)*100;
-      tries++;
-    }
-    tries=0;
-    int last_temp2 = sensors_oneWire.getTempCByIndex(1)*100;
-    while((last_temp2==0||last_temp2==-12700) && tries<max_tries){
-      last_temp2 = sensors_oneWire.getTempCByIndex(1)*100;
-      tries++;
-    }
-=======
-    sensor_oneWire.requestTemperatures();
-	  setParameter(PARAM_TEMPERATURE_EXT, sensor_oneWire.getTempCByIndex(0)*100);
->>>>>>> a930e0411fd1ecdc6e6a83ca4c9a458da155c72a
+    sensors_oneWire.requestTemperatures();
 
     // vTaskDelay(1000);
    // Serial.println(String("TEMP_DALLAS1 = " + last_temp1));
    // Serial.println(String("TEMP_DALLAS2 = " + last_temp2));
    //     Serial.println("TEST1");    
-	  setParameter(PARAM_TEMPERATURE_EXT, last_temp1);
+	  setParameter(PARAM_TEMPERATURE_EXT, sensors_oneWire.getTempCByIndex(0)*100);
   vTaskDelay(200);
-	  setParameter(PARAM_TEMPERATURE_EXT2, last_temp2);
+	  setParameter(PARAM_TEMPERATURE_EXT2, sensors_oneWire.getTempCByIndex(1)*100);
       vTaskDelay(1000);
         
   }
